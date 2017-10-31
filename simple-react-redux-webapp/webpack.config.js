8ff587d1cbd1b894891892ec6e7d778751ce7abc
Aziz Syed <aziz.syed@nike.com>
2017-10-30 23:01:35 -0700

    Simplification

--- simple-react-redux-webapp/webpack.config.js
+++ simple-react-redux-webapp/webpack.config.js
@@ -4,51 +4,33 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 // const autoprefixer = require('autoprefixer');
 // const CleanPlugin = require('./utils/clean-plugin');
 // cons3000);
 
-
-const NodeService = {
-  isProduction: () => false,
-};
+const isProduction = process.env['NODE_ENV'] === 'production';
 
 const config = {
   output: {
-    path: path.join(__dirname, '../dist'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
   },
   plugins: [
-    // new CleanPlugin({
-    //     files: ['dist/*']
-    // }),
     // new ExtractTextPlugin('css/bundle.css'),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new webpack.LoaderOptionsPlugin({
-    //     options: {
-    //         postcss: [
-    //             autoprefixer({
-    //                 browsers: ['last 2 version']
-    //             })
-    //         ]
-    //     }
-    // }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../src/index.html'),
-      inject  : 'body',
+      template: path.resolve(__dirname, 'src/index.html'),
+      inject: 'body',
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-        // APP_CONFIG: JSON.stringify(
-        //   port: 3000,
-        // )
-      }
-    })
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
   ],
   module: {
     exprContextCritical: false, // Suppress "The request of a dependency is an expression"
@@ -56,28 +38,23 @@ const config = {
       {
         test: /\.(js|jsx)$/,
         loaders: 'babel-loader',
-        include: path.join(__dirname, '../src'),
+        include: path.join(__dirname, 'src'),
       },
-      // {
-      //   test: /\.scss$/,
-      //   loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
-      //   include: path.join(__dirname, 'src'),
-      // },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
         loader: 'url-loader?limit=10000&name=[name]-[hash].[ext]',
         include: path.join(__dirname, 'src'),
       },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-        include: path.join(__dirname, 'src'),
-      },
+      // {
+      //   test: /\.json$/,
+      //   loader: 'json-loader',
+      //   include: path.join(__dirname, 'src'),
+      // },
     ],
   },
 };
 
-if (NodeService.isProduction()) {
+if (isProduction) {
   config.entry = './src/Bootstrap';
   config.plugins.push(new UglifyJSPlugin());
 } else {
