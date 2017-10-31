10a30395982366f1664702f55924b028fe15dab6
Aziz Syed <aziz.syed@nike.com>
2017-10-30 23:01:35 -0700

    Simplification

--- simple-react-redux-webapp/server.js
+++ simple-react-redux-webapp/server.js
@@ -1,50 +1,35 @@
-'use strict';
-
-// const config = require('./config/config');
-
-const NodeService = {
-  isProduction: () => false,
-};
-
-// const { example } = config;
-// if (!example) throw new Error('configuration cannot be null/undefined');
+const isProduction = process.env['NODE_ENV'] === 'production';
 
 const PORT = 3000;
 
-if (NodeService.isProduction()) {
-    const express = require('express');
-    const path = require('path');
+if (isProduction) {
+  const express = require('express');
+  const path = require('path');
 
-    const app = express();
+  const app = express();
 
-    // Configure static resources
-    app.use(
-        express.static(
-            path.join(__dirname, '/dist')
-        )
-    );
+  // Configure static resources
+  app.use(express.static(path.join(__dirname, '/dist')));
 
-    // Configure server-side routing
-    app.get('*', (req, res) => {
-        const dist = path.join(
-            __dirname, '/dist/index.html'
-        );
-        res.sendFile(dist);
-    });
+  // Configure server-side routing
+  app.get('*', (req, res) => {
+    const dist = path.join(__dirname, '/dist/index.html');
+    res.sendFile(dist);
+  });
 
-    // Open socket
-    app.listen(PORT, () => {
-        console.log(`Started Express server on port ${PORT}`);
-    });
+  // Open socket
+  app.listen(PORT, () => {
+    console.log(`Started Express server on port ${PORT}`);
+  });
 } else {
-    const webpack = require('webpack');
-    const WebpackDevServer = require('webpack-dev-server');
-    const config = require('./config/webpack.dev.config');
+  const webpack = require('webpack');
+  const WebpackDevServer = require('webpack-dev-server');
+  const config = require('./webpack.config');
 
-    new WebpackDevServer(webpack(config), {
-        hot               : true,
-        historyApiFallback: true
-    }).listen(PORT, 'localhost', error => {
-        console.log(error || `Started WebpackDevServer on port ${PORT}`);
-    });
+  new WebpackDevServer(webpack(config), {
+    hot: true,
+    historyApiFallback: true,
+  }).listen(PORT, 'localhost', (error) => {
+    console.log(error || `Started WebpackDevServer on port ${PORT}`);
+  });
 }
